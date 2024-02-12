4f6529482054e891705be338035ef1d7f209bee3
jlambert360 <39280243+jlambert360@users.noreply.github.com>
2024-02-12 15:43:05 -0500

    Version bump to 2.5.1

--- build-appimage.sh
+++ build-appimage.sh
@@ -61,7 +61,7 @@ rm ./AppDir/usr/lib/libglib*
 rm ./AppDir/usr/lib/libgobject*
 
 # Bake an AppImage with the update metadata
-export VERSION="2.5"
+export VERSION="2.5.1"
 UPDATE_INFORMATION="${ZSYNC_STRING}" \
 	./Tools/linuxdeploy-update-plugin --appdir=./AppDir/
 
